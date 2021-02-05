1a2871791bd91435d749f7ae1093680345007167
sitri <stefano02.itri@gmail.com>
2021-02-05 09:03:18 +0100

    added index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Stefano Itri</title>
+    <!-- weitere Kopfinformationen -->
+    <!-- Kommentare werden im Browser nicht angezeigt. -->
+</head>
+<body>
+<h1>Stefano Itri</h1>
+</body>
+</html>
